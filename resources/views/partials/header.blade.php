94c51b9ff02c68792f2c9b81401ff60e6cc56e23
Vito Pizzulli <127297685+vito-pizzulli@users.noreply.github.com>
2023-07-28 18:06:09 +0200

    Initialize refatcory_of_style

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+<header class="mb-5">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid ">
+            <a class="navbar-brand" href="#">
+                Laravel Shores
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav w-100 justify-content-end">
+                    <a class="nav-link" href="{{route('admin.shores.index')}}">Laraver Shores</a>
+                    <a class="nav-link" href="{{route('admin.shores.create')}}">Add New Shore</a>
+                </div>
+          </div>
+        </div>
+      </nav>
+</header>
